01f2238fb6aa1049edb46acbd4e3a293816b9c06
Sam Mangham <mangham@gmail.com>
2022-01-27 22:42:24 +0000

    Rolled in TheDeer's styling

--- src/App.vue
+++ src/App.vue
@@ -95,62 +95,54 @@ export default {
       "missions": [
         {
           "slug": "001",
-          "name": "Bug-Hunt",
+          "name": "Deep-Impact",
           "status": "start"
         },
       ],
       "pilots": [
         {
-          "callsign": "Jinzo",
-          "alias": "Shayan Arsenal",
-          "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Type - Worst Beat"
+          "callsign": "Juggler",
+          "alias": "Kaboosh Jones",
+          "code": "0143a844-73ce-4dc1-b8e6-84dc1d1514e8//NDL-C-SINGULARITY-DIVER",
+          "corpro": "SSC",
+          "frame": "Mourning Cloak",
+          "mech": "Stabsy1"
         },
         {
-          "callsign": "Kal",
-          "alias": "Vier Flau",
-          "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Necrophile"
+          "callsign": "Deluge",
+          "alias": "Neke Dakster",
+          "code": "ca48b587-cb32-4923-b50d-444b1daf8683//NDL-C-FIRST-MANTLE",
+          "corpro": "IPS-N",
+          "frame": "Tortuga",
+          "mech": "Habitual Jester"
         },
         {
-          "callsign": "Starboard",
-          "alias": "Maria",
-          "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Scarlet Judicator"
+          "callsign": "Firebird",
+          "alias": "Kira",
+          "code": "cd37f093-45d8-4fc9-8b58-38ef1dae0f8f//NDL-C-SATELLITE-HIDE",
+          "corpro": "Horus",
+          "frame": "Pegasus",
+          "mech": "Incomprehensible Differences"
         },
         {
-          "callsign": "Badlands",
-          "alias": "Marlene Gatsby",
-          "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Mayfly"
-        },
-        {
-          "callsign": "Compadre",
-          "alias": 'Clint "CC" Cruz',
-          "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Rio Bravo"
+          "callsign": "Mammoth",
+          "alias": "Heinzhermann Anderson",
+          "code": "af670ef1-5cd1-497e-ad57-9a9daeeca771//NDL-C-LUNAR-TOWER",
+          "corpro": "HA",
+          "frame": "Iskander",
+          "mech": "Express Delivery"
         },
       ],
       "header": {
-        "planet": "Hercynia",
+        "planet": "Asteroid NI-341B",
         "year": "5014u",
-        "system": "Ardennes-3",
-        "gate": "Atlas-Quanokrim",
-        "ring": "Atlas-Line",
-        "headerTitle": "Mirrorsmoke",
-        "headerSubtitle": "Mercenary Company",
-        "subheaderTitle": "Crisis Response",
-        "subheaderSubtitle": "Delta-Echo-Echo-Zulu",
+        "system": "New Hampshire",
+        "gate": "Cascade-Shasta",
+        "ring": "Cascade-Line",
+        "headerTitle": "Union Navy",
+        "headerSubtitle": "Auxiliary Program",
+        "subheaderTitle": "Superchargers",
+        "subheaderSubtitle": "Mercenary Company",
       },
       "options":{
         "eventsMarkdownPerMission": true
